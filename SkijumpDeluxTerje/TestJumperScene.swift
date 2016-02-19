64801569a0e483317722921fcd47ecea18d98577
Daniel Haugstvedt <danielhaugstvedt@daniels-macbook-pro-2.local>
2016-02-19 21:36:27 +0100

    Made a jumper and a test scene to test it in

---
+++ SkijumpDeluxTerje/TestJumperScene.swift
@@ -0,0 +1,14 @@
+//
+//  TestJumperScene.swift
+//  SkijumpDeluxTerje
+//
+//  Created by Daniel Haugstvedt on 19/02/16.
+//  Copyright © 2016 Daniel Haugstvedt. All rights reserved.
+//
+
+
+import SpriteKit
+
+class TestJumperScene: SKScene {
+    
+}
