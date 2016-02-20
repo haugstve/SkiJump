5a529cb2379d8751ffd216d4958b5fcd40dd8b04
Daniel Haugstvedt <danielhaugstvedt@daniels-macbook-pro-2.local>
2016-02-20 08:50:02 +0100

    Added physics categories

--- SkijumpDeluxTerje/TestJumperScene.swift
+++ SkijumpDeluxTerje/TestJumperScene.swift
@@ -9,6 +9,28 @@
 
 import SpriteKit
 
+protocol CustomNodeEvents {
+  func didMoveToScene()
+}
+
+struct PhysicsCategory {
+    static let Node:UInt32 = 0
+    static let Jumpter:UInt32 = 0b1 //1
+    static let Hill:UInt32 = 0b100 //2
+}
+
 class TestJumperScene: SKScene {
+
+  override func didMoveToView(view: SKView) {
+    enumerateChildNodesWithName("//*", usingBlock: {node, _ in
+      print("node added")
+      if let customNode = node as? CustomNodeEvents {
+        customNode.didMoveToScene()
+      }
+      if let sprite = node as? SKSpriteNode {
+        sprite.color = SKColor.greenColor()
+      }
+    })
+  }
     
 }
