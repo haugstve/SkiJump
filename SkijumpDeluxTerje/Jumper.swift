65c9df44363df440b6e4ae97f6eae67101a2af7a
Daniel Haugstvedt <daniel.haugstvedt@gmail.com>
2016-02-20 13:14:05 +0100

    The joint is improving but it is still hard to get the anchor right

--- SkijumpDeluxTerje/Jumper.swift
+++ SkijumpDeluxTerje/Jumper.swift
@@ -20,9 +20,13 @@ class Jumper: SKSpriteNode, CustomNodeEvents {
     upperArm = childNodeWithName("upper_arm") as! SKSpriteNode
     head = childNodeWithName("head") as! SKSpriteNode
     
-    let shoulder = SKPhysicsJointFixed.jointWithBodyA(self.physicsBody!, bodyB: upperArm.physicsBody!, anchor: anchorFromNode(upperArm))
-    scene?.physicsWorld.addJoint(shoulder)
-    print("\(anchorFromNode(upperArm))")
+//    let shoulder = SKPhysicsJointFixed.jointWithBodyA(self.physicsBody!, bodyB: upperArm.physicsBody!, anchor: anchorFromNode(upperArm))
+    let shoulder2 = SKPhysicsJointPin.jointWithBodyA(self.physicsBody!, bodyB: upperArm.physicsBody!, anchor: anchorFromNode(upperArm))
+    scene?.physicsWorld.addJoint(shoulder2)
+    print("anchor point \(anchorFromNode(upperArm))")
+    print("upper arm rotation \(upperArm.zRotation)")
+     print("size of frame \(upperArm.frame.size)")
+     print("size of rotated frame \(nodeSize(upperArm))")
     
   }
     
@@ -33,6 +37,17 @@ class Jumper: SKSpriteNode, CustomNodeEvents {
         guard let parentNode = sprite.parent else {
             return CGPointZero
         }
-        return scenOfSprite.convertPoint(CGPointZero, fromNode: parentNode)
+        var pointOfJointInParentCoordiantes = sprite.position
+        pointOfJointInParentCoordiantes += CGPoint(x: -10, y: 5)
+        //Angle is relative to parent and the body is tilted
+        //pointOfJointInParentCoordiantes += CGPoint(angle: sprite.zRotation) * nodeSize(sprite).height/2
+        // 0 is paralell and when the arms are raised in fromt the ange is more than 90 degrees
+        return scenOfSprite.convertPoint(pointOfJointInParentCoordiantes, fromNode: parentNode)
+    }
+    
+    func nodeSize(sprite: SKNode) -> CGSize {
+        let spriteCopy = sprite.copy() as! SKNode
+        spriteCopy.zRotation = 0
+        return spriteCopy.frame.size
     }
 }
